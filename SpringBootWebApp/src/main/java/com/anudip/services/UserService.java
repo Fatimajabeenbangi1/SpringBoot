81a6d51ebb240985e14d51a8554c0f2e5956c0a4
Fatimajabeenbangi1 <167059243+Fatimajabeenbangi1@users.noreply.github.com>
2024-10-13 11:02:58 +0530

    Delete SpringBootWebApp directory

--- SpringBootWebApp/src/main/java/com/anudip/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.anudip.services;
-
-import com.anudip.entities.User;
-
-public interface UserService 
-{
-  public boolean registerUser(User user);
-  public User loginUser(String email, String password);
-}
