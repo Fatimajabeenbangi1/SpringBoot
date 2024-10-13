546912733a112b94a48cc9dde759bbcac0ec90d4
Fatimajabeenbangi1 <167059243+Fatimajabeenbangi1@users.noreply.github.com>
2024-10-13 11:04:02 +0530

    Add files via upload

---
+++ SpringBootWebApp/src/main/resources/java/com/anudip/services/UserService.java
@@ -0,0 +1,9 @@
+package com.anudip.services;
+
+import com.anudip.entities.User;
+
+public interface UserService 
+{
+  public boolean registerUser(User user);
+  public User loginUser(String email, String password);
+}
